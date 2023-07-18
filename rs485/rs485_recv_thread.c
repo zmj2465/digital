e06fad189ccca1f73817a7e1f74592157ae02046
zmj2465 <2465400103@qq.com>
2023-07-18 17:37:44 +0800

    test

--- rs485/rs485_recv_thread.c
+++ rs485/rs485_recv_thread.c
@@ -17,10 +17,15 @@ void* rs_485_recv_thread(void* arg)
     pthread_detach(pthread_self());
 
     char get[MAX_DATA_LEN];
-
+    rs485_info_t* p = (rs485_info_t*)get;
     while (1)
     {
-
+        switch (p->type)
+        {
+            case SP:
+                enqueue_no_block(&info.thread_queue[DATA_SEND_THREAD], p->sp, SP_LEN);
+                break;
+        }
     }
 }
 
--- rs485/rs485_recv_thread.h
+++ rs485/rs485_recv_thread.h
@@ -9,6 +9,18 @@
 #include "queue.h"
 
 
+enum SystemState {
+    POWER_ON = 0,
+    SELF_TEST = 1,
+    REFUELING = 2,
+    PRE_SEPARATION = 3,
+    SEPARATION = 4,
+    ESTABLISHING_LINK = 5,
+    BREAKING_LINK = 6,
+    RECONNECTION = 7,
+    SP = 0x60
+};
+
 #define SP_LEN sizeof(sp_info_t)
 
 #pragma pack(1)
@@ -24,14 +36,25 @@ typedef struct {
     uint8_t  dst;
     uint8_t  src;
     uint8_t  type;
-    uint8_t  sp[SP_LEN];
-    uint16_t crc;
-    uint32_t end;
+    union {
+        uint8_t  sp[SP_LEN];
+        uint16_t crc;
+        uint32_t end;
+    };
+    uint16_t crc_;
+    uint32_t end_;
 }rs485_info_t;
 #pragma pack()
 
 
+
+
+
+
 void* rs_485_recv_thread(void* arg);
+
+
+
 
 
 
